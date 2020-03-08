a009fda7c83f3efdc12dd6b36aa39daf65f5d032
Nathaniel Hall <nathaniel.hall@gmail.com>
2020-03-08 04:18:40 -0400

    remove layout

--- src/application.jsx
+++ src/application.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
-import { Layout } from './layout'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { MapContainer } from 'components/map-container'
 
 export const Application = () => {
-  return <Layout></Layout>
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const getLocations = async () => {
+      const response = await axios.get(
+        'https://data.nasa.gov/resource/y77d-th95.json'
+      )
+      setData(response.data)
+    }
+
+    getLocations()
+  }, [])
+
+  return (
+    <div>
+      <main>{data && <MapContainer data={data} />}</main>
+    </div>
+  )
 }
