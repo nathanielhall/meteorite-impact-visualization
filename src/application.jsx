b2fde391219823b9f2a41bf4ee5615326dae1288
Nathaniel Hall <nathanielhall@Nathaniels-MBP.home>
2020-03-09 04:58:14 -0400

    organize input components

--- src/application.jsx
+++ src/application.jsx
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Map } from 'components/map'
 import { Header } from 'components/header'
-import { DateRange } from 'components/date-range'
+// import { DateRange } from 'components/date-range'
 
 export const Application = () => {
   const [data, setData] = useState()
-  const [startDate, setStartDate] = useState(new Date('01/01/2010'))
-  const [endDate, setEndDate] = useState(new Date('03/01/2020'))
+  // const [startDate, setStartDate] = useState(new Date('01/01/2010'))
+  // const [endDate, setEndDate] = useState(new Date('03/01/2020'))
 
-  const onSubmit = (values) => {
-    setStartDate(values.start)
-    setEndDate(values.end)
-  }
+  // const onSubmit = (values) => {
+  //   setStartDate(values.start)
+  //   setEndDate(values.end)
+  // }
 
+  // TODO: Need more info on how the fitering should work
+  // TODO: Should the data ever re-import? Provide an import button? Import after filter?...
   useEffect(() => {
     const getLocations = async () => {
       const response = await axios.get(
@@ -28,11 +30,11 @@ export const Application = () => {
   return (
     <div>
       <Header title="Meteorite Impact Locations">
-        <DateRange
+        {/* <DateRange
           startDate={startDate}
           endDate={endDate}
           onSubmit={onSubmit}
-        />
+        /> */}
       </Header>
       <main>{data && <Map data={data} />}</main>
     </div>
