b81d7dff2c0c137ae27daa0be8551382d71f85f2
NATHANIEL HALL <NCHALL@ATTAIN.COM>
2020-03-09 15:25:32 -0400

    local storage / refactor

--- src/components/map/map-popup/map-popup.jsx
+++ src/components/map/map-popup/map-popup.jsx
@@ -1,51 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { TextBox } from 'components/inputs/text-box'
 import { DatePicker } from 'components/inputs/date-picker'
-import { useLocalStorateState } from 'components/local-storage'
 
 export const MapPopup = ({ location }) => {
-  const [values, setValues] = useLocalStorateState(location.id, location)
+  // const [values, setValues] = useState(location)
 
-  const handleChange = (event) => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
+  // const handleChange = (event) => {
+  //   const target = event.target
+  //   const value = target.value
+  //   const name = target.name
 
-    setValues((prevState) => ({
-      ...prevState,
-      [name]: value
-    }))
-  }
+  //   setValues((prevState) => ({
+  //     ...prevState,
+  //     [name]: value
+  //   }))
+  // }
 
-  const { name, year, nametype, recclass, mass, fall } = values
+  const { id, name, year, nametype, recclass, mass, fall } = location
   return (
     <form noValidate>
-      <TextBox name="name" label="Name" onChange={handleChange} value={name} />
-      <DatePicker
-        name="year"
-        label="Year"
-        onChange={handleChange}
-        value={year}
-        format="yyyy"
-        views={['year']}
-        autoOk
-        disableFuture
-      />
-      <TextBox
-        name="nametype"
-        label="Type"
-        onChange={handleChange}
-        value={nametype}
-      />
-      <TextBox
-        name="recclass"
-        label="Recclass"
-        onChange={handleChange}
-        value={recclass}
-      />
-      <TextBox name="mass" label="Mass" onChange={handleChange} value={mass} />
-      <TextBox name="fall" label="Fall" onChange={handleChange} value={fall} />
+      <LocalStorageFormControl id={id}>
+        <TextBox name="name" label="Name" value={name} />
+      </LocalStorageFormControl>
+
+      <LocalStorageFormControl id={id}>
+        <DatePicker
+          name="year"
+          label="Year"
+          value={year}
+          format="yyyy"
+          views={['year']}
+          autoOk
+          disableFuture
+        />
+      </LocalStorageFormControl>
+
+      <LocalStorageFormControl id={id}>
+        <TextBox name="nametype" label="Type" value={nametype} />
+      </LocalStorageFormControl>
+
+      <LocalStorageFormControl id={id}>
+        <TextBox name="recclass" label="Recclass" value={recclass} />
+      </LocalStorageFormControl>
+
+      <LocalStorageFormControl id={id}>
+        <TextBox name="mass" label="Mass" value={mass} />
+      </LocalStorageFormControl>
+
+      <LocalStorageFormControl id={id}>
+        <TextBox name="fall" label="Fall" value={fall} />
+      </LocalStorageFormControl>
     </form>
   )
 }
@@ -67,3 +72,43 @@ MapPopup.propTypes = {
     })
   })
 }
+
+const callAll = (...fns) => (...args) => fns.forEach((fn) => fn && fn(...args))
+
+const LocalStorageFormControl = ({
+  id,
+  children,
+  formControl = React.Children.only(children),
+  lsKey = `${id}:${formControl.props.name}`
+}) => {
+  const [value, setValue] = useState(() => {
+    let value
+    try {
+      value =
+        JSON.parse(window.localStorage.getItem(lsKey)) ||
+        formControl.props.value
+    } catch (e) {
+      value = formControl.props.value
+    }
+    return value
+  })
+
+  React.useEffect(() => {
+    if (value) {
+      window.localStorage.setItem(lsKey, JSON.stringify(value))
+    } else {
+      window.localStorage.removeItem(lsKey)
+    }
+  }, [value, lsKey])
+
+  return React.cloneElement(formControl, {
+    onChange: callAll(formControl.props.onChange, (e) =>
+      setValue(e.target.value)
+    ),
+    value
+  })
+}
+
+// useEffect(() => {
+//   window.localStorage.setItem(key, JSON.stringify(state))
+// }, [state])
