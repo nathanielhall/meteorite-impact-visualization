507f092e2367be503dbd81af4be3c9036e8bf3cb
Nathaniel Hall <nathaniel.hall@gmail.com>
2020-03-10 11:11:54 -0400

    filter by date range

--- src/components/date-range/date-range.jsx
+++ src/components/date-range/date-range.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
-import { DatePicker } from 'components/date-picker'
+import { DatePicker } from 'components/inputs/date-picker'
 import Button from '@material-ui/core/Button'
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Grid
+} from '@material-ui/core'
 
-export const DateRange = ({ startDate, endDate, onSubmit }) => {
+export const DateRange = ({ startDate, endDate, onSubmit, onClose }) => {
   const [values, setValues] = useState({
     start: startDate,
     end: endDate
@@ -21,35 +28,51 @@ export const DateRange = ({ startDate, endDate, onSubmit }) => {
 
   const { start, end } = values
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-        onSubmit(values)
-      }}
-    >
-      <DatePicker
-        name="start"
-        label="Start"
-        onChange={handleChange}
-        value={start}
-        views={['year']}
-        autoOk
-        disableFuture
-      />
+    <Dialog open>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          onSubmit(values)
+        }}
+      >
+        <DialogContent>
+          <DialogContentText>
+            Filter meteorite impact locations
+          </DialogContentText>
 
-      <DatePicker
-        name="end"
-        label="End"
-        onChange={handleChange}
-        value={end}
-        views={['year']}
-        autoOk
-        disableFuture
-      />
+          <Grid container direction="column" justify="space-evenly">
+            <Grid item>
+              <DatePicker
+                name="start"
+                label="Start"
+                onChange={handleChange}
+                value={start}
+                views={['year']}
+                autoOk
+                disableFuture
+              />
+            </Grid>
+            <Grid item>
+              <DatePicker
+                name="end"
+                label="End"
+                onChange={handleChange}
+                value={end}
+                views={['year']}
+                autoOk
+                disableFuture
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
 
-      <Button color="inherit" type="submit">
-        Filter
-      </Button>
-    </form>
+        <DialogActions>
+          <Button color="inherit" type="submit">
+            Filter
+          </Button>
+          <Button onClick={onClose}>Close</Button>
+        </DialogActions>
+      </form>
+    </Dialog>
   )
 }
