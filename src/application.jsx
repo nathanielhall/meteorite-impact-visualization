0d812b0aeb72f58967e4310db24ec6701f056a8e
NATHANIEL HALL <NCHALL@ATTAIN.COM>
2020-03-10 11:11:54 -0400

    filter by date range

--- src/application.jsx
+++ src/application.jsx
@@ -4,18 +4,19 @@ import { Map } from 'components/map'
 import { Header } from 'components/header'
 import Button from '@material-ui/core/Button'
 import { UserEdits } from 'components/user-edits'
-// import { DateRange } from 'components/date-range'
+import { DateRange } from 'components/date-range'
 
 export const Application = () => {
   const [data, setData] = useState()
-  // const [startDate, setStartDate] = useState(new Date('01/01/2010'))
-  // const [endDate, setEndDate] = useState(new Date('03/01/2020'))
+  const [startDate, setStartDate] = useState(new Date('01/01/2010'))
+  const [endDate, setEndDate] = useState(new Date('03/01/2020'))
   const [showUserEdits, setShowUserEdits] = useState(false)
+  const [showDateRangeFilter, setShowDateRangeFilter] = useState(false)
 
-  // const onSubmit = (values) => {
-  //   setStartDate(values.start)
-  //   setEndDate(values.end)
-  // }
+  const onSubmit = (values) => {
+    setStartDate(values.start)
+    setEndDate(values.end)
+  }
 
   // TODO: Need more info on how the fitering should work
   // TODO: Should the data ever re-import? Provide an import button? Import after filter?...
@@ -33,18 +34,23 @@ export const Application = () => {
   return (
     <div>
       <Header title="Meteorite Impact Locations">
-        {/* <DateRange
-          startDate={startDate}
-          endDate={endDate}
-          onSubmit={onSubmit}
-        /> */}
         <Button color="inherit" onClick={() => setShowUserEdits(true)}>
           Edits
         </Button>
-        <Button color="inherit">Filter</Button>
+        <Button color="inherit" onClick={() => setShowDateRangeFilter(true)}>
+          Filter
+        </Button>
       </Header>
       <main>{data && <Map data={data} />}</main>
       {showUserEdits && <UserEdits onClose={() => setShowUserEdits(false)} />}
+      {showDateRangeFilter && (
+        <DateRange
+          startDate={startDate}
+          endDate={endDate}
+          onSubmit={onSubmit}
+          onClose={() => setShowDateRangeFilter(false)}
+        />
+      )}
     </div>
   )
 }
